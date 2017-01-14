bb2bee8d2f62cee02eac0537bd299fc1a75cb45b
enafly <emuratspah1@etf.unsa.ba>
2017-01-14 23:06:54 +0100

    Spirala4

--- positiveVibes/Spirala1/js/indexJS.js
+++
@@ -1,9 +0,0 @@
-function prikazi(id){
-	var m = document.getElementById(id);
-	if(m.style.display== 'block' || m.style.display== ''){
-		m.style.display= 'block';
-	}
-	else{
-		m.style.display== 'none';
-	}
-}
